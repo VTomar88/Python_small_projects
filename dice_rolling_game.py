14f095116d46e07dea4928b922b93a1d035d8497
Varsha Tomar <varshatomar88@gmail.com>
2024-11-14 18:26:49 -0500

    Added program information in dice_rolling_game.py

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,3 +1,8 @@
+# This is dice rolling game
+# Varsha Tomar 14 Nov 2024
+# In this game if user says yes, the program will generate two numbers of a dice
+# else reponse is generated if user didn't responded 'y' or 'n'
+# If user input is 'n' then program exits
 from random import randint
 while True:
     user_input = input("Roll the dice? (y/n): ")
