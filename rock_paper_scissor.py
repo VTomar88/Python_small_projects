bc2aae1e50b7a8f79886299564845f23a13c51db
Varsha Tomar <varshatomar88@gmail.com>
2024-11-15 07:59:31 -0500

    refactored rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -9,44 +9,69 @@ item_list = ('r', 'p', 's')
 emojis = {"r": "🪨", "p": "📄", "s": "✄"}
 
 # counter for scoring
-wins = 0
-loses = 0
-ties = 0
+game_wins = 0
+game_loses = 0
+game_ties = 0
 
-# Loop
-while True:
-    # Ask user to choose from (r)ock, (p)aper, (s)cissor
-    user_input = input('Rock, Paper, Scissor? (r/p/s): ')
-    
-    # Computer to choose between r, p, s
-    comp_guess = random.choices(item_list)[0]
+def display_choices(user_input, comp_guess):
+    # print what user choose
+    print(f'You chose {emojis[user_input]}')   
+        # print what computer choose
+    print(f'Computer choose {emojis[comp_guess]}')
 
-    # if user = r or p or s
-    if user_input in item_list:
-        # print what user choose
-        print(f'You chose {emojis[user_input]}')   
-         # print what computer choose
-        print(f'Computer choose {emojis[comp_guess]}')
-        # if user = computer -> tie
-        if comp_guess == user_input:
-            print("Tie!")
-            ties += 1
-        # else if user = p/r/s and user r/s/p 
-        elif ((user_input=='p' and comp_guess=='r') or 
-              (user_input=='r' and comp_guess=='s') or 
-              (user_input=='s' and comp_guess=='p')):
-            print('You Win')
-            wins += 1
-        else:
-            print('You Lose')
-            loses += 1
-    # else invalid option
+def compare_results(user_input, comp_guess):
+    global game_loses, game_wins, game_ties
+    # if user = computer -> tie
+    if comp_guess == user_input:
+        print("Tie!")
+        game_ties += 1
+    # else if user = p/r/s and user r/s/p 
+    elif ((user_input=='p' and comp_guess=='r') or 
+        (user_input=='r' and comp_guess=='s') or 
+        (user_input=='s' and comp_guess=='p')):
+        print('You Win')
+        game_wins += 1
     else:
-        print('Invalice chocie!')
-        continue # skip rest if the choice is invlid and reach the top level of the loop
+        print('You Lose')
+        game_loses += 1
+
+def game_continue_ask():
+    global game_wins, game_loses, game_ties
     # wish to continue -  no break
-    continue_ask = input('Continue? (y/n): ').lower()
-    if continue_ask == 'n':
-        # Display the final score
-        print(f'\nFinal score: Wins: {wins}, Loses: {loses}, Ties: {ties}')
-        break
+    while True:
+        continue_ask = input('Continue? (y/n): ').lower()
+        if continue_ask == 'n':
+            # Display the final score
+            print(f'\nFinal score: Wins: {game_wins}, Loses: {game_loses}, Ties: {game_ties}')
+            return False
+        elif continue_ask == 'y':
+            return True
+        else:
+            print("Enter only 'y' for yes or 'n' for no.")
+
+def compare_user_input(comp_guess):
+    while True:
+        # Ask user to choose from (r)ock, (p)aper, (s)cissor
+        user_input = input('Rock, Paper, Scissor? (r/p/s): ') 
+        # if user = r or p or s
+        if user_input in item_list:
+            display_choices(user_input, comp_guess)
+            compare_results(user_input, comp_guess)
+            break      
+        # else invalid option
+        else:
+            print('Invalice chocie!')
+
+def play_game():
+    while True:               
+        # Computer to choose between r, p, s
+        comp_guess = random.choices(item_list)[0]
+        # compare the resuts and play the game
+        compare_user_input(comp_guess)
+       # ask user if wish to continue the game
+        if not game_continue_ask():
+            break
+
+# Play the game
+if __name__ == "__main__":
+    play_game()
