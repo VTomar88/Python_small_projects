3358f110ff75244944b7d3d027bd8294b304c16c
Varsha Tomar <varshatomar88@gmail.com>
2024-11-22 12:42:19 -0500

    added nested_loop_challenge.py

---
+++ nested_loop_challenge.py
@@ -0,0 +1,12 @@
+"""
+This is nested loop challenge. Using nested loop print in the following format
+xxxx
+xx
+xxxx
+xx
+xx
+"""
+for x_count in range(2):
+    print('xxxx')
+    for _ in range(x_count+1):
+        print('xx')
